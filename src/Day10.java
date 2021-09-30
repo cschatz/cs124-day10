e7e0035eed6555a692bf7be7aac874f20e37aedf
Colin Schatz <cschatz@mills.edu>
2021-09-30 09:46:53 -0700

    Update

--- src/Day10.java
+++ src/Day10.java
@@ -1,8 +1,35 @@
+import java.util.ArrayList;
 
 public class Day10 {
 
-	public static void permutations(String s) {
+	private static void pHelper(String sofar, String remaining) {
+		// Base case (solution is ready!)
+		if (remaining.isEmpty()) {
+			// print the solution
+			System.out.println(sofar);
+		}
 		
+		// Recursive step
+		// For all possible choices, pick ONE choice
+		// and add it to sofar		
+		for (int i = 0; i < remaining.length(); i++) {
+			// Store the current element
+			// We need to remove this from remain and
+			//  add it to sofar
+			char current = remaining.charAt(i);
+			// The arguments to the recursive call need to be:
+			// * sofar becomes sofar + current
+			// * remaining becomes remaining OMITTING character #i
+			pHelper(sofar + current, 
+					remaining.substring(0, i) + remaining.substring(i+1));
+		}
+		
+	}
+	
+	
+	public static void permutations(String s) {
+		// spawn the first copy of pHelper
+		pHelper("", s);
 	}
 	
 	public static void subsets(String s) {
@@ -10,7 +37,7 @@ public class Day10 {
 	}
 	
 	public static void main(String[] args) {
-		
+		permutations("ABCD");
 	}
 
 }
